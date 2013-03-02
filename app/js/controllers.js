f8ad4253f733cb841493881f03aa7d105281c43f
James Anselm <james@anselm.ca>
2013-03-02 10:09:08 -0500

    some other changes, pie calendar

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -28,28 +28,30 @@ function LoginCtrl($scope, $location) {
 
 function RegisterCtrl($scope, $location) {
   var user = new Parse.User(); 
+  $scope.registerStepImgPath = "img/register-step1.png";
+
   $scope.registerUser_page1 = function(register1){
     var error;
     if(register1) {
-      if(register1.firstName)
-       user.set("firstName", register1.firstName);
-      if(register1.lastName)
-       user.set("lastName", register1.lastName);
+      if(register1.name)
+       user.set("name", register1.name);
       if(register1.password && register1.confirmPassword && register1.password.length > 5 && register1.password == register1.confirmPassword)
        user.set("password", register1.password);
       else
        error = "the password entries weren't valid."
-      if(register1.email && register1.confirmEmail && register1.email == register1.confirmEmail) {
+      if(register1.email) {
        user.set("email", register1.email);
        user.set("username", register1.email);
       }
       else
-       error = "the email entries weren't valid."
+       error = "the email entry wasn't valid."
     }
     else {
       error = "please fill in the registration form";
     }
     if(!error){
+      $scope.registerStepImgPath = "img/register-step2.png";
+      //loadPieChart();
       $('#register_page1').addClass('inactive');
       $('#register_page2').removeClass('inactive');
       $('#register_page3').addClass('inactive');
@@ -82,11 +84,16 @@ function RegisterCtrl($scope, $location) {
     else
       error = "other time is invalid";
 
+    if(Number(register2.travelTime) + Number(register2.sleepTime) + Number(register2.eatingTime) + Number(register2.personalTime) + Number(register2.otherTime) >= 24) {
+      error = "more than the full day is allocated. Please adjust the times."
+    }
+
     if(!error) {
       user.signUp(null, {
         success: function(user) {
           // Hooray! Let them use the app now.
            $scope.$apply(function(){
+             $scope.registerStepImgPath = "img/register-step3.png";
              $('#register_page1').addClass('inactive');
              $('#register_page2').addClass('inactive');
              $('#register_page3').removeClass('inactive');
@@ -103,17 +110,17 @@ function RegisterCtrl($scope, $location) {
   }
 
   $scope.toDashboard = function() {
+    $location.path('/dashboard');
     $('#register_page1').removeClass('inactive');
     $('#register_page2').addClass('inactive');
     $('#register_page3').addClass('inactive');
-    $location.path('/dashboard');
   }
 
   $scope.toCreateProject = function() {
+    $location.path('/createProject');
     $('#register_page1').removeClass('inactive');
     $('#register_page2').addClass('inactive');
     $('#register_page3').addClass('inactive');
-    $location.path('/createProject');
   }
 }
 
@@ -121,8 +128,24 @@ function CreateProjectCtrl($scope, $location){
 
 }
 
+function loadPieChart() {
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(function() { drawChart(); });
+}
 
+/*function drawChart(t_trans, t_sleep, t_eat, t_personal, t_other) {
+  var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Transportation', t_trans],
+          ['Sleep', t_sleep],
+          ['Eat', t_eat],
+          ['Watch TV', t_personal],
+          ['Sleep', t_other]
+      ]);
 
+      var options = {
+      };
 
-
-
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+}*/
